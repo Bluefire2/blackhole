12af017d5539109a1c240eb005779c43cf83d6b3
Kyrylo Chernyshov <chernyshov.k@gmail.com>
2025-11-30 23:33:08 -0800

    Add github-pages config

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // This must match the GitHub repository name so that
+  // assets are served correctly from GitHub Pages.
+  // Your site will be available at:
+  // https://<username>.github.io/blackhole/
+  base: '/blackhole/',
+});
+
+
