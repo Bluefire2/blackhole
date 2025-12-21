d517c42d9aaa8e09168e75e578e2f1d85787d194
Kyrylo Chernyshov <chernyshov.k@gmail.com>
2025-12-20 22:37:47 -0800

    Set cname directive to publish to custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // This must match the GitHub repository name so that
-  // assets are served correctly from GitHub Pages.
-  // Your site will be available at:
-  // https://<username>.github.io/blackhole/
-  base: '/blackhole/',
+  // Root for blackhole.kyrylo.lol
+  base: '/',
 });
 
 
